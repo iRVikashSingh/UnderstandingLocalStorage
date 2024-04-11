dff79ca0d501b8c8bb1492d70719cbc9f24d1b85
iRVikashSingh <vksrajput.sde@gmail.com>
2024-04-11 22:59:41 +0530

    html and js file added

---
+++ index.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+  localStorage.setItem("Username", username);
+  localStorage.setItem("Email", email);
+  localStorage.setItem("Phone", phone);
+});
+
+//this line not required
+module.exports = handleFormSubmit;
